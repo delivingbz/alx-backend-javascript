0d6e820762566c5ce0612a483333aec50d5f3aa6
delivingbz <kingscode2@gmail.com>
2023-01-25 21:11:09 +0000

    block-scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+export default function taskBlock(trueOrFalse) {
+  
+  const task;
+  const task2;
+  	
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
